a1065e45b94a88478b13c14e87bbea37ec6ec532
itkazmi814 <itkazmi814@gmail.com>
2017-11-20 17:41:14 -0600

    wip

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 // if (process.env.NODE_ENV === "production") {
 // 	app.use(express.static("./client/public/index"));
 // }
-app.use("/static", express.static(path.join(__dirname, "client/build")));
+app.use("/static", express.static(path.join(__dirname, "./client/public")));
 
 //Add routes
 require("./routes/api-routes")(app);
