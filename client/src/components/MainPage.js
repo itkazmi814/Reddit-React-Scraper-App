bfa3d9478371a70cd966f499c22385e186fbcd14
itkazmi814 <itkazmi814@gmail.com>
2017-11-19 18:19:40 -0600

    Search now modularized subcomponent

--- client/src/components/MainPage.js
+++ client/src/components/MainPage.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import API from "../utils/API";
 import Article from "./Article";
 import Jumbotron from "./Jumbotron";
+import Search from "./Search";
 
 class MainPage extends Component {
 	state = {
@@ -32,7 +33,6 @@ class MainPage extends Component {
 
 	handleSearchSubmit = event => {
 		event.preventDefault();
-		console.log("Searching for topic: ", this.state.search.topic);
 		API.searchForArticles(this.state.search.topic).then(response => {
 			this.setState({ articles: response.data });
 		});
@@ -42,34 +42,11 @@ class MainPage extends Component {
 		return (
 			<div>
 				<Jumbotron handleScrapeArticles={this.handleScrapeArticles} />
-				<form class="col-6">
-					<div class="form-group d-inline-block">
-						<label for="exampleInputEmail1">Topic</label>
-						<input
-							value={this.state.search.topic}
-							onChange={this.handleInputChange}
-							name="topic"
-							class="form-control"
-							id="exampleInputEmail1"
-							aria-describedby="emailHelp"
-							placeholder="Enter topic"
-						/>
-					</div>
-					<div class="form-group d-inline-block">
-						<label for="exampleInputPassword1">Subreddit</label>
-						<input
-							value={this.state.search.subreddit}
-							onChange={this.handleInputChange}
-							name="subreddit"
-							class="form-control"
-							id="exampleInputPassword1"
-							placeholder="Enter subreddit"
-						/>
-					</div>
-					<button class="btn btn-primary" onClick={this.handleSearchSubmit}>
-						Search
-					</button>
-				</form>
+				<Search
+					search={this.state.search}
+					handleInputChange={this.handleInputChange}
+					handleSearchSubmit={this.handleSearchSubmit}
+				/>
 
 				{this.state.articles.map((article, i) => (
 					<Article key={i} {...article} />
--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,7 +1,37 @@
 import React from "react";
 
 const Search = props => (
-
+	<form class="col-6">
+		<div class="form-group d-inline-block">
+			<label for="exampleInputEmail1">Topic</label>
+			<input
+				value={props.search.topic}
+				onChange={props.handleInputChange}
+				name="topic"
+				class="form-control"
+				id="exampleInputEmail1"
+				aria-describedby="emailHelp"
+				placeholder="Enter topic"
+			/>
+		</div>
+		<div class="form-group d-inline-block">
+			<label for="exampleInputPassword1">Subreddit</label>
+			<input
+				value={props.search.subreddit}
+				onChange={props.handleInputChange}
+				name="subreddit"
+				class="form-control"
+				id="exampleInputPassword1"
+				placeholder="Enter subreddit"
+			/>
+		</div>
+		<button
+			class="btn btn-primary d-inline-block"
+			onClick={props.handleSearchSubmit}
+		>
+			Search
+		</button>
+	</form>
 );
 
 export default Search;
